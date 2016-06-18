3f4edf44badcb07851f9b28caade8e4d4df95779
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-06-18 15:41:44 -0300

    added myfile.h

--- myfile.h
+++ myfile.h
@@ -18,46 +18,28 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * @file   myfile.c
+ * @file   myfile.h
  * @author Carlos Alexandro Becker (author@mail.com)
  *
  * @brief  Some Brief.
  */
-#include <jni.h>
-#include <stdio.h>
+#ifndef FUNCTIONS_H_BIND_INCLUDED
+#define FUNCTIONS_H_BIND_INCLUDED
 
 /**
- * Blah blah blah.
- * @param obj The obj
- * @return True
+ * Blah blah blah
+ * @param env Java Environment
+ * @param obj Obj
+ * @param number Number
+ * @retval void
  */
-static bool
-something_has_blah (jobject * obj)
-{
-  return true;
-}
+void my_fn (JNIEnv * env, jobject * obj, int number);
 
 /**
  * Blah blah blah
  * @param env Java Environment
  * @param obj Object
- * @param name Char buffer
- * @return Number of bytes
  */
-static int
-blah_name (JNIEnv * env, jobject * obj, char *name)
-{
-  return 1
-}
+void my_fn2 (JNIEnv * env, jobject * obj);
 
-void
-my_fn (JNIEnv * env, jobject * obj, int number)
-{
-   // code
-}
-
-void
-my_fn2 (JNIEnv * env, jobject * obj)
-{
-
-}
+#endif
